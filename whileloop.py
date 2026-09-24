369b9d025916b20c518829eaf0c9cdabb4040910
hyarelgits <110811148+hyarelgits@users.noreply.github.com>
2026-09-24 17:41:38 +0530

    Fix while loop condition and increment logic

--- whileloop.py
+++ whileloop.py
@@ -13,3 +13,5 @@ while 100:
         i += 1
     else:
         break
+
+hyarel-HU5H4tL3E2c-tYx909rpe-r7f_jkXtACngCUDzSwUB59dpiA_joO9m-2NZ3YPrtJ6vr8BBQT3BlbkFJVAal2QwThJxRUMuXyALFnuKoztmfVCQPdLCV3poGbXnFLgPIrC3XzsfdpxnNKQL6IgOGqMqfsA
